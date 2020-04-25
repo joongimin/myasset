eeb58e753edf4d5c1bb5976c965b8edb65880f2d
Joon Min <joongi.min@gmail.com>
2020-04-25 10:31:59 -0400

    FEATURE - cap client:setup, cap client:deploy
    ### Desc
    - `cap client:setup` - run `npm install`
    - `cap client:deploy` - run `npm run build` and create production build in to dist directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,3 +3,23 @@ lock '~> 3.13.0'
 set :application, 'myasset'
 set :repo_url, 'git@github.com:joongimin/myasset.git'
 set :deploy_to, "~/#{fetch(:application)}"
+set :linked_dirs, %w[myasset-client/node_modules]
+
+namespace :client do
+  task :setup do
+    on roles(:app) do
+      within "#{release_path}/myasset-client" do
+        execute :npm, 'install'
+      end
+    end
+  end
+
+  task :deploy do
+    on roles(:app) do
+      within "#{release_path}/myasset-client" do
+        execute :npm, 'run', 'build'
+      end
+    end
+  end
+  after 'deploy:published', 'client:deploy'
+end
