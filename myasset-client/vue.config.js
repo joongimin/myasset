ca4deabb0c619eb180309e9a4c04b5abf2e7f075
Joon Min <joongi.min@gmail.com>
2020-04-25 15:31:08 -0400

    FEATURE - support vue-i18n SFC
    ### Desc
    - support i18n block in SFC

--- myasset-client/vue.config.js
+++ myasset-client/vue.config.js
@@ -9,5 +9,12 @@ module.exports = {
       .rule('svg-sprite')
       .use('svgo-loader')
       .loader('svgo-loader');
+
+    config.module
+      .rule('i18n')
+      .resourceQuery(/blockType=i18n/)
+      .type('javascript/auto')
+      .use('i18n')
+      .loader('@intlify/vue-i18n-loader');
   }
 };
