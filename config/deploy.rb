3c0c4f4ce82ca20ab14f4a878c1abfed18f217db
Joon Min <joongi.min@gmail.com>
2020-04-25 12:59:50 -0400

    FEATURE - Implemented myasset-client deploy
    ### Desc
    - In local environment
      - run `npm run build` to build production files
      - compress files to tarball
      - upload tarball to server
    - In web server
      - uncompress tarball
      - delete it

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,24 +2,30 @@ lock '~> 3.13.0'
 
 set :application, 'myasset'
 set :repo_url, 'git@github.com:joongimin/myasset.git'
-set :deploy_to, "~/#{fetch(:application)}"
-set :linked_dirs, %w[myasset-client/node_modules]
+set :deploy_to, "/home/ubuntu/#{fetch(:application)}"
 
 namespace :client do
-  task :setup do
-    on roles(:app) do
-      within "#{release_path}/myasset-client" do
-        execute :npm, 'install'
+  DIST_FILE = 'dist.tar.gz'.freeze
+
+  task :build do
+    run_locally do
+      within 'myasset-client' do
+        execute :npm, 'run', 'build'
+        execute :tar, '-czf', "#{DIST_FILE} -Cdist ."
       end
     end
   end
 
   task :deploy do
     on roles(:app) do
-      within "#{release_path}/myasset-client" do
-        execute :npm, 'run', 'build'
+      within current_path do
+        upload! "myasset-client/#{DIST_FILE}", DIST_FILE
+        execute :mkdir, '-p', 'public'
+        execute :tar, '-xzf', DIST_FILE, '-Cpublic'
+        execute :rm, DIST_FILE
       end
     end
   end
-  after 'deploy:published', 'client:deploy'
 end
+after 'deploy:published', 'client:build'
+after 'client:build', 'client:deploy'
