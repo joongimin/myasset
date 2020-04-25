8882799ae68ab4331850228a9ce2e76c6765eda0
Joon Min <joongi.min@gmail.com>
2020-04-25 15:24:05 -0400

    FEATURE - svg-sprite
    ### Desc
    - Let's sprite SVGs
    - Add svgo, svgo-loader, vue-cli-plugin-svg-sprite

--- myasset-client/vue.config.js
+++ myasset-client/vue.config.js
@@ -3,5 +3,11 @@ module.exports = {
     host: 'lmyasset.live',
     port: 9710,
     public: 'http://lmyasset.live'
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('svg-sprite')
+      .use('svgo-loader')
+      .loader('svgo-loader');
   }
 };
